f300b6e864c7db5b84b94e2f86549639a0099be8
EvertonFabricio <everton.5by5@gmail.com>
2022-03-08 10:09:03 -0300

    Adicionar arquivos de projeto.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ant = 0, atual = 1, fib = 0, i = 0;
+
+
+            Console.WriteLine("Digite um número inteiro e positivo: ");
+            int parada = int.Parse(Console.ReadLine());
+
+            while (i < parada)
+            {
+                Console.Write($"{fib}, ");
+                ant = atual;
+                atual = fib;
+                fib = atual + ant;
+
+
+                i++;
+            }
+        }
+    }
+}
